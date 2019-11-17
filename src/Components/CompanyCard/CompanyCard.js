d08b95116c7bef9dc42f79fd49272a41940939c6
psycho-valker <rameez.connect@gmail.com>
2019-11-17 09:26:07 +0530

    company card populated using props and maps

--- src/Components/CompanyCard/CompanyCard.js
+++ src/Components/CompanyCard/CompanyCard.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './CompanyCard.css';
 
-class CompanyCard extends Component {
-    render() {
+const CompanyCard = props => {
         return (
                 <div className={styles.card}>
                     <div className={styles.cardInner}>
@@ -10,33 +9,26 @@ class CompanyCard extends Component {
                                 <div className={styles.cardHeader}>
                                     <img className={styles.logo}  src={require('./cimpress.png')} alt="company"/>
                                     <div className={styles.desc}>
-                                        <p>Cimpress</p>
-                                        <p>Rating!!!</p>
+                                        <p>{props.companyName}</p>
+                                        <p>{props.companyRating}</p>
                                     </div>
                                 </div>
 
                                 <div className={styles.cardContent}>
-                                    <p>11 CTC</p>
-                                    <p>54 Students placed so far!</p>
+                                    <p>{props.companyCTC}</p>
+                                    <p>{props.companyPlaced} Students placed so far!</p>
                                     <p>See Interview Experiences</p>
                                 </div>
                         </div>
 
                         <div className={styles.cardBack}>
                                 <div className={styles.backDesc}>
-                                    <p>
-                                        Cimpress N.V. is a Dutch company which invests in and operates a wide variety
-                                        of businesses which use mass customization to configure and produce small 
-                                        quantities of individually customized goods. Those products are sold to small 
-                                        businesses, graphic designers and consumers through a 
-                                        number of customer-facing brands that Cimpress operates.
-                                    </p>
+                                    <p>{props.companyAbout}</p>
                                 </div>
                         </div>
                     </div>
                 </div>
         );
-    }
 }
 
 export default CompanyCard;
--- src/Containers/Companies/Companies.js
+++ src/Containers/Companies/Companies.js
@@ -4,26 +4,54 @@ import CompanyCard from '../../Components/CompanyCard/CompanyCard';
 
 
 const Companies = () => {
-        
-        return (
+    const data = {
+        cmp1: {
+            id: "ansbd",
+            Name: "Cimpress",
+            Rating: "4.4",
+            CTC: "11 LPA",
+            Placed: "4",
+            About: " Cimpress N.V. is a Dutch company which invests in and operates a wide variety of businesses which use mass customization to configure and produce small quantities of individually customized goods. Those products are sold to small businesses, graphic designers and consumers through a number of customer-facing brands that Cimpress operates."
+        },
+        cmp2: {
+            id: "adjksd",
+            Name: "Cimpress",
+            Rating: "4.4",
+            CTC: "11 LPA",
+            Placed: "4",
+            About: " Cimpress N.V. is a Dutch company which invests in and operates a wide variety of businesses which use mass customization to configure and produce small quantities of individually customized goods. Those products are sold to small businesses, graphic designers and consumers through a number of customer-facing brands that Cimpress operates."
+        },
+        cmp3: {
+            id: "adjdaksd",
+            Name: "Cimpress",
+            Rating: "4.4",
+            CTC: "11 LPA",
+            Placed: "4",
+            About: " Cimpress N.V. is a Dutch company which invests in and operates a wide variety of businesses which use mass customization to configure and produce small quantities of individually customized goods. Those products are sold to small businesses, graphic designers and consumers through a number of customer-facing brands that Cimpress operates."
+        },
+        cmp4: {
+            id: "adjkssadd",
+            Name: "Cimpress",
+            Rating: "4.4",
+            CTC: "11 LPA",
+            Placed: "4",
+            About: " Cimpress N.V. is a Dutch company which invests in and operates a wide variety of businesses which use mass customization to configure and produce small quantities of individually customized goods. Those products are sold to small businesses, graphic designers and consumers through a number of customer-facing brands that Cimpress operates."
+        }
 
-                <section className={styles.cards}>
-                    <CompanyCard />
-                    <CompanyCard />
-                    <CompanyCard />
-                    <CompanyCard />
-                    <CompanyCard />
-                    <CompanyCard />
-                    <CompanyCard />
-                    <CompanyCard />
-                    <CompanyCard />
-                    <CompanyCard />
-                    <CompanyCard />
-                    <CompanyCard />
-                </section>
-
-            
-    
+    }
+    return (
+        <section className={styles.cards}>
+            {Object.keys(data).map((cmp) =>
+                <CompanyCard 
+                    key = {data[cmp].id}
+                    companyName={data[cmp].Name}
+                    companyRating = {data[cmp].Rating}
+                    companyCTC = {data[cmp].CTC}
+                    companyPlaced = {data[cmp].Placed}
+                    companyAbout = {data[cmp].About}
+                />
+            )}
+        </section> 
         );
 }
 
